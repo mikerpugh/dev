f2a41ea50c436321d5dd8f25da392c9283875753
mikerpugh <mikerpugh@gmail.com>
2021-05-18 22:05:16 -0600

    first push

---
+++ contracts/MyToken.sol
@@ -0,0 +1,17 @@
+//this is a single line comment
+
+/* this 
+is 
+a 
+multi line comment
+*/
+
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor () public ERC20 ("MyToken", "MTKN"){
+        _mint(msg.sender, 1000000000000000000)
+    }
+}
