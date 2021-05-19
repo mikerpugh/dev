c78b2aab22ee2ed0ea48dc187923ee0bb0b95ec0
mikerpugh <mikerpugh@gmail.com>
2021-05-18 22:44:55 -0600

    openzeppelin

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -6,12 +6,13 @@ a
 multi line comment
 */
 
-pragma solidity ^0.6.2;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
     constructor () public ERC20 ("MyToken", "MTKN"){
-        _mint(msg.sender, 1000000000000000000)
+        _mint(msg.sender, 1000000000000000000);
     }
 }
