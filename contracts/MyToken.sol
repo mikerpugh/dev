ba0753a3f707c7ead84872a766eb46486807ba91
mikerpugh <mikerpugh@gmail.com>
2021-05-18 22:46:55 -0600

    zeppelin

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -8,7 +8,7 @@ multi line comment
 
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
