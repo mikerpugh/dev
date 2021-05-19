df83f1605dac8635b15066ce9296d74b09cfc4db
mikerpugh <mikerpugh@gmail.com>
2021-05-18 23:07:15 -0600

    typo fix

--- migrations/2_deploy_Tokens.js
+++ migrations/2_deploy_Tokens.js
@@ -1,6 +1,6 @@
 const MyToken = artifacts.require("MyToken")
 
-module.exports = async functin (deployer, network, accounts) {
+module.exports = async function (deployer, network, accounts) {
     //Deploy MyToken
     await deployer.deploy(MyToken)
     const myToken = await MyToken.deployed()
